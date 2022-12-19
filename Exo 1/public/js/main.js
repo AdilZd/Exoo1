b0c2421881a62fa0d3f334c244bc9983eea4079c
AdilZd <nowayat0me@gmail.com>
2022-12-19 11:40:40 +0100

    Exo 1

---
+++ Exo 1/public/js/main.js
@@ -0,0 +1,9 @@
+let prenom = " Adil ";
+console.log(prenom)
+
+let number = 22;
+console.log (number);
+
+let presentation = " Je m'appelle Adil et j'ai 22 ans ";
+console.log (presentation)
+alert (presentation)
